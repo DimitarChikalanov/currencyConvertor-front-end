8551d842ba6af4f8423c425d00861c9a3ca7689d
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2020-12-11 15:25:33 +0200

    Configured routes for the landing page. Works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: LandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
