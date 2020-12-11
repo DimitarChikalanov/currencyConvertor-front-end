e27f705d17801680dfd5958cd8322b1ed53ee72e
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2020-12-11 16:49:03 +0200

    Styled the header, footer and landing page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { HeaderComponent } from './theme/shared/header/header.component';
 import { FooterComponent } from './theme/shared/footer/footer.component';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,10 @@ import { FooterComponent } from './theme/shared/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbCollapseModule,
+    NgbModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
