b6df4fd058c7948e14ae86294a1bc040063e38a3
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2021-01-04 13:37:34 +0200

    Currency already exists fixed

--- src/app/components/admin-components/add-new-currency/add-new-currency.component.ts
+++ src/app/components/admin-components/add-new-currency/add-new-currency.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import NewCurrency from 'src/app/entities/NewCurrency';
 import { AdminService } from 'src/app/services/admin-service/admin.service';
+import { CurrencyService } from 'src/app/services/currency-service/currency.service';
 
 @Component({
   selector: 'app-add-new-currency',
@@ -23,6 +24,15 @@ export class AddNewCurrencyComponent implements OnInit, OnDestroy {
   /* Is request successful. */
   isSuccessful: boolean = false;
 
+  /* All currencies list */
+  allCurrencyList: String[];
+  
+  /* Error message */
+  errorCurrencyExist = '';
+
+  /* */
+  addCurrencyFailed = false;
+
   /**
    * @constructor
    * 
@@ -30,7 +40,8 @@ export class AddNewCurrencyComponent implements OnInit, OnDestroy {
    * @param fb
    */
   constructor(private adminService: AdminService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private currencyService: CurrencyService) { }
   
   /**
    * Get the controls of the register form.
@@ -41,11 +52,15 @@ export class AddNewCurrencyComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
 
+    this.allCurrencyList = [];
+
     /* Form build and validations. */
     this.addNewCurrencyForm = this.fb.group({
       currencyName: ['', [Validators.required]],
       rate: ['', [Validators.required]]
     })
+
+    this.allCurrencyList = this.getCurrencies();
   }
 
   /**
@@ -56,6 +71,13 @@ export class AddNewCurrencyComponent implements OnInit, OnDestroy {
 
     this.newCurrency = this.addNewCurrencyForm.value; // assign the form value to the new currency object
 
+    /* If the currency to add already exists in the list. */
+    if (this.allCurrencyList.includes(this.newCurrency.currencyName)) {
+      this.errorCurrencyExist = 'Currency already exists';
+      this.addCurrencyFailed = true;
+      return;
+    }
+
     /* Send the post request to the backend. */
     this.subscriptions.add(this.adminService.addNewCurrency(this.newCurrency).subscribe(data => {
 
@@ -63,6 +85,24 @@ export class AddNewCurrencyComponent implements OnInit, OnDestroy {
     }))
 
     this.redirectHomeAfterRegistration(); // redirect to home
+  }
+
+  /**
+   *  This method subscribes to the result of the GET request (getAllCurrencies),
+   *  Takes the data which is returned as a response.
+   *  Takes all the names of the currencies and records them in allCurrencyNames.
+   * 
+   *  @return allCurrencyNames - the array of names which is now filled with names
+   */
+  getCurrencies(): String[] {
+    this.currencyService.getAllCurrencies().subscribe((data) => {
+      // keep all currency names, for example ["BGN", "EUR"] etc.
+      data.forEach((oneCurrency) => {
+        this.allCurrencyList.push(oneCurrency?.nameOfValue);
+      })
+    });
+
+    return this.allCurrencyList;
   }
 
   // After a successful adding of currency, redirect the user to the home page
